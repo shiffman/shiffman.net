d71ed3423bb915d153b46a832556e0e1c834c68e
phucduong86 <phuc.duongdinh@gmail.com>
2019-04-10 21:23:04 -0400

    updated nav section to be keyboard accessible

--- js/main.js
+++ js/main.js
@@ -1,4 +1,62 @@
+
+
 $(document).ready(function(){
+	var MainNav = {};
+
+	MainNav.App = (function() {
+
+	var ESCAPE_CODE = 27;
+
+	var navButton = $('#menu-button'),
+		navMenu = $('#global-nav');
+	
+	var navLinks = navMenu.find('a');
+
+	function initApp() {
+		navMenu.on('keydown', handleKeydown);
+		navButton.on('click', handleClick);
+		disableNavLinks();
+	}
+	function handleKeydown(event) {
+		if (event.keyCode === ESCAPE_CODE) {
+		document.body.classList.toggle('active');
+		disableNavLinks();
+		navButton.focus();
+		}
+	}
+	function handleClick(event) {
+		if (document.body.classList.contains('active')) {
+		document.body.classList.remove('active');
+		document.getElementById('menu-button').classList.remove('active');
+		disableNavLinks();
+		}
+		else {
+		document.body.classList.add('active');
+		document.getElementById('menu-button').classList.add('active');
+		enableNavLinks();
+		navLinks.eq(0).focus();
+		}
+	}
+	function enableNavLinks() {
+		navButton.attr('aria-label', 'Close navigation menu');
+		navButton.attr('aria-expanded', 'true');
+		navMenu.removeAttr('aria-hidden');
+		navLinks.removeAttr('tabIndex');
+	}
+	function disableNavLinks() {
+		navButton.attr('aria-label', 'Open navigation menu');
+		navButton.attr('aria-expanded', 'false');
+		navMenu.attr('aria-hidden', 'true');
+		navLinks.attr('tabIndex', '-1');
+	}
+
+	return {
+		init: function(){
+		initApp();
+		}
+	}
+	})();
+	new MainNav.App.init();
 
 	$('.left-container, .right-container').removeClass('out');
 
